d285216154b14f55aae19ae4c7c39ec64a8a6560
Mew <54446552+Mayamee@users.noreply.github.com>
2021-08-26 11:52:40 +0300

    Update log2JSON.js

--- log2JSON.js
+++ log2JSON.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const { sortJSON } = require("./sort");
+const path = require("path");
 
 const PATTERN = {
   APP: "(?=(Сведения|Ошибка|Предупреждение))(.+?)(?=(Сведения|Ошибка|Предупреждение))",
@@ -43,41 +44,48 @@ const parseSEC = (textFromFile, regexPattern) =>
       };
     });
 
-const SRC_PATH = process.argv[2];
+const targetFolders = fs
+  .readdirSync("./src", { withFileTypes: true })
+  .filter((d) => d.isDirectory())
+  .map((e) => e.name);
+// вычисляем целевые папки
 
-try {
-  const dataApp =
-    fs.readFileSync(`./src/${SRC_PATH}/app.txt`, "utf-8") + "Сведения";
-  const toJSONApp = JSON.stringify(
-    sortJSON(parse(dataApp, PATTERN.APP)),
-    null,
-    2
-  );
-  fs.writeFileSync(`./dist/${SRC_PATH}/app.json`, toJSONApp);
-} catch (error) {
-  console.error(error);
-}
-try {
-  const dataSec =
-    fs.readFileSync(`./src/${SRC_PATH}/security.txt`, "utf-8") + "Аудит успеха";
-  const toJSONSec = JSON.stringify(
-    sortJSON(parseSEC(dataSec, PATTERN.SECURITY)),
-    null,
-    2
-  );
-  fs.writeFileSync(`./dist/${SRC_PATH}/security.json`, toJSONSec);
-} catch (error) {
-  console.error(error);
-}
-try {
-  const dataSys =
-    fs.readFileSync(`./src/${SRC_PATH}/system.txt`, "utf-8") + "Сведения";
-  const toJSONSys = JSON.stringify(
-    sortJSON(parse(dataSys, PATTERN.SYSTEM)),
-    null,
-    2
-  );
-  fs.writeFileSync(`./dist/${SRC_PATH}/system.json`, toJSONSys);
-} catch (error) {
-  console.error(error);
-}
+targetFolders.forEach((e) => {
+  fs.mkdirSync(`./dist/${e}`, { recursive: true });
+  try {
+    const dataApp = fs.readFileSync(`./src/${e}/app.txt`, "utf-8") + "Сведения";
+    const toJSONApp = JSON.stringify(
+      sortJSON(parse(dataApp, PATTERN.APP)),
+      null,
+      2
+    );
+    fs.writeFileSync(`./dist/${e}/app.json`, toJSONApp);
+  } catch (error) {
+    console.error(error);
+  }
+  try {
+    const dataSec =
+      fs.readFileSync(`./src/${e}/security.txt`, "utf-8") + "Аудит успеха";
+    const toJSONSec = JSON.stringify(
+      sortJSON(parseSEC(dataSec, PATTERN.SECURITY)),
+      null,
+      2
+    );
+    fs.writeFileSync(`./dist/${e}/security.json`, toJSONSec);
+  } catch (error) {
+    console.error(error);
+  }
+  try {
+    const dataSys =
+      fs.readFileSync(`./src/${e}/system.txt`, "utf-8") + "Сведения";
+    const toJSONSys = JSON.stringify(
+      sortJSON(parse(dataSys, PATTERN.SYSTEM)),
+      null,
+      2
+    );
+    fs.writeFileSync(`./dist/${e}/system.json`, toJSONSys);
+  } catch (error) {
+    console.error(error);
+  }
+});
+// создаем папки в каталоге dist и наполняем содержимым
